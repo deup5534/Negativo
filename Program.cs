fb25655682e1927d88d042aa03745c874022c236
deup5534 <thiagaonaruto@gmail.com>
2025-01-28 16:24:08 -0300

    Finalizado.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Início do exercício.
+
+Console.Clear();
+
+System.Console.WriteLine("Seja bem vindo(a) ao exercício 'Negativo'!\n");
+
+Console.ForegroundColor = ConsoleColor.Gray;
+System.Console.WriteLine("aperte qualquer tecla para continuar...");
+Console.ReadKey();
+Console.ResetColor();
+
+// O exercício em si.
+Console.Clear();
+
+int NumeroUsuario;
+
+System.Console.WriteLine("Digite um número, que vou dizer se ele é negativo ou não.\n");
+System.Console.WriteLine("Número:");
+NumeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+if (NumeroUsuario < 0)
+{
+    System.Console.WriteLine("Você digitou um número negativo.");
+}
+
+else
+{
+    System.Console.WriteLine("Você digitou um número positivo.");
+}
